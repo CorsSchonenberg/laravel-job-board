c6d242a8f4e8bc4b63d7b52ba36694aca4e15734
Cors Schonenberg <s1135206@student.hsleiden.nl>
2023-08-04 15:40:00 +0200

    fetched all jobs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\JobController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('jobs', JobController::class)
+    ->only(['index']);
